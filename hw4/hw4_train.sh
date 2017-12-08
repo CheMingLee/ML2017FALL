4a09bd30f0dcfd670167941f7c9d1e26bfada7b7
CheMingLee <r05522621@ntu.edu.tw>
2017-12-08 21:44:22 +0800

    Add files via upload

--- hw4/hw4_train.sh
+++ hw4/hw4_train.sh
@@ -1,1 +1,3 @@
+wget -O w2v_model.syn1neg.npy 'https://github.com/CheMingLee/ML2017FALL/releases/download/model/w2v_model.syn1neg.npy'
+wget -O w2v_model.wv.syn0.npy 'https://github.com/CheMingLee/ML2017FALL/releases/download/model/w2v_model.wv.syn0.npy'
 python hw4_train.py $1 $2
