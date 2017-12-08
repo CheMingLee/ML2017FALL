d539203621df9495b4496716de44b27c4174df50
CheMingLee <r05522621@ntu.edu.tw>
2017-12-08 21:56:56 +0800

    hw4

--- hw4/hw4_test.sh
+++ hw4/hw4_test.sh
@@ -1,3 +1,3 @@
-wget -O w2v_model.syn1neg.npy 'https://github.com/CheMingLee/ML2017FALL/releases/download/model/w2v_model.syn1neg.npy'
-wget -O w2v_model.wv.syn0.npy 'https://github.com/CheMingLee/ML2017FALL/releases/download/model/w2v_model.wv.syn0.npy'
+wget -O w2v_model.syn1neg.npy 'https://www.dropbox.com/s/fod2w7hb7bpdw8u/w2v_model.syn1neg.npy?dl=1'
+wget -O w2v_model.wv.syn0.npy 'https://www.dropbox.com/s/agtcw63bu71zk4y/w2v_model.wv.syn0.npy?dl=1'
 python hw4_test.py $1 $2
