1abbfe1f28ad85a9d055ef5613e84a071b1320fb
CheMingLee <r05522621@ntu.edu.tw>
2018-01-08 15:57:22 +0800

    Add files via upload

--- hw6/pca.py
+++ hw6/pca.py
@@ -13,18 +13,16 @@ for i in range(415):
 
 all_imgs = np.array(all_imgs, dtype=np.float)
 
-X = all_imgs.T
-X_mean = np.mean(X, axis=0)
-U, s, V = np.linalg.svd(X - X_mean, full_matrices=False)
+X_mean = np.mean(all_imgs, axis=0)
+X_norm = all_imgs - X_mean
+U, s, V = np.linalg.svd(X_norm.T, full_matrices=False)
 
 image = io.imread(os.path.join(imgs_path, img))
 orgi_shape = np.shape(image)
-image = np.array(image.flatten(), dtype=np.float)
+X = np.array(image.flatten(), dtype=np.float)
 
-X = image.T
-X_mean = np.mean(X, axis=0)
 k = 4
-weight = np.dot(X, U[:,:k])
+weight = np.dot(X - X_mean, U[:,:k])
 reconMat = np.dot(U[:,:k], weight) + X_mean
 
 M = reconMat.T.reshape(orgi_shape)
